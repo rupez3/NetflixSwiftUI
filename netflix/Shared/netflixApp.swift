977b13854df29f8fa7ed49ec07d321052876f681
Rupesh Chhetri <Rupesh_Chhetri@cable.comcast.com>
2022-01-17 10:43:34 -0500

    projet setup

---
+++ netflix/Shared/netflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixApp.swift
+//  Shared
+//
+//  Created by Rupesh Chhetri on 1/17/22.
+//
+
+import SwiftUI
+
+@main
+struct netflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
